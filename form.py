24732b40e85701357c7763aed4d289b55401260d
giarroccoaj <giarroccoaj@g.cofc.edu>
2016-03-30 12:42:06 -0400

    Update form.py

--- form.py
+++ form.py
@@ -6,9 +6,9 @@ from wtforms import TextField, SubmitField, validators
 
 class ContactForm(Form):
     first_name = TextField("First Name:", [validators.DataRequired("Please enter a First Name.")])
-    last_name = TextField("Last Name:", [validators.Required("Please enter a Last Name.")])
-    birth_date = TextField("Birth Date:", [validators.Required("Please enter a Birth Date.")])
-    id = TextField("ID:", [validators.Required("Please enter a ID. ")])
+    last_name = TextField("Last Name:", [validators.DataRequired("Please enter a Last Name.")])
+    birth_date = TextField("Birth Date:", [validators.DataRequired("Please enter a Birth Date.")])
+    id = TextField("ID:", [validators.DataRequired("Please enter a ID. ")])
     trading_partner_id = TextField("Trading Partner ID:",
-                                   [validators.Required("Please enter a Trading Partner Id.")])
+                                   [validators.DataRequired("Please enter a Trading Partner Id.")])
     submit = SubmitField("Submit")
